ab0e2576eb557cbb7cd1dfca3b529602a19c98d4
Yueyang Zheng <yueyangz@seas.upenn.edu>
2016-10-06 00:10:22 -0400

    Removing console.log

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -10,14 +10,10 @@ module.exports = {
 	getTemp: function(location) {
 		var encodedLocation = encodeURIComponent(location);
 		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
-		console.log(requestUrl);
 		return axios.get(requestUrl).then(function(res) {
 			var cityFromResponse = res.data.name.toLowerCase().trim().replace(" ", "");
 			var cityFromUser = decodeURI(encodedLocation.toLowerCase().trim()).replace(" ", "");
-			// cityFromUser = cityFromUser.replace("%20", " ");
 			if (cityFromUser !== cityFromResponse) {
-				console.log(cityFromResponse);
-				console.log(cityFromUser);
 				throw new Error(`Can you make my life a little easier? I think you are a ${cityFromUser}!`);
 			}
 			if (res.data.cod && res.data.message) {
