1d4590633f614eaed9a6fc8637471f443cc634b4
Yueyang Zheng <yueyangz@seas.upenn.edu>
2016-10-06 00:06:49 -0400

    Fixing API bug where user can type random stuff and gets irrelevant response

--- public/bundle.js
+++ public/bundle.js
@@ -25187,12 +25187,22 @@
 	var axios = __webpack_require__(229);
 	var OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=07bd99558d851e113067bbf72926ec66&units=metric';
 
+	var checkCityName = function checkCityName(string1, string2) {};
+
 	module.exports = {
 		getTemp: function getTemp(location) {
 			var encodedLocation = encodeURIComponent(location);
 			var requestUrl = OPEN_WEATHER_MAP_URL + '&q=' + encodedLocation;
 			console.log(requestUrl);
 			return axios.get(requestUrl).then(function (res) {
+				var cityFromResponse = res.data.name.toLowerCase().trim().replace(" ", "");
+				var cityFromUser = decodeURI(encodedLocation.toLowerCase().trim()).replace(" ", "");
+				// cityFromUser = cityFromUser.replace("%20", " ");
+				if (cityFromUser !== cityFromResponse) {
+					console.log(cityFromResponse);
+					console.log(cityFromUser);
+					throw new Error('Can you make my life a little easier? I think you are a ' + cityFromUser + '!');
+				}
 				if (res.data.cod && res.data.message) {
 					throw new Error(res.data.message);
 				} else {
